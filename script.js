67a7b0b42dd1e9f1930910c23e4d7795127c17b0
harngkae <166604547+harngkae@users.noreply.github.com>
2024-11-28 15:02:44 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function changeGreeting() {
-    const greetings = ["欢迎再次访问！", "希望你有美好的一天！", "感谢您的光临！"];
-    const randomIndex = Math.floor(Math.random() * greetings.length);
-    document.getElementById('greeting').innerText = greetings[randomIndex];
-}
